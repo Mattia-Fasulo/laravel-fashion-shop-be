02b5c5ab0be1a28f474a95dd48d5cccd0d952eb6
mattia.rollo <ste.tia@hotmail.it>
2023-01-23 21:09:34 +0100

    test images, add comments in product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
@@ -16,9 +17,13 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        //il codice sotto va decommentato se si vuole evitare il blocco della foreignKey 
+        // DB::statement("SET foreign_key_checks = 0;");
+        // Product::truncate();
+
         $products = config('dataseeder.products');
 
-        foreach($products as $product) {
+        foreach ($products as $product) {
             $new_product = new Product();
             $new_product->type_id = $product['category_id'];
             $new_product->texture_id = $product['texture_id'];
@@ -31,16 +36,19 @@ class ProductSeeder extends Seeder
             $new_product->rating = $product['rating'];
             $new_product->cover_image = ProductSeeder::storeimage($product['api_featured_image']);
             $new_product->save();
+
+            // DB::statement("SET foreign_key_checks = 1;");
         }
     }
 
-    public static function storeimage($img){
-        $url = 'https:'.$img;
+    public static function storeimage($img)
+    {
+        $url = 'https:' . $img;
         $contents = file_get_contents($url);
         $temp_name = substr($url, strrpos($url, '/') + 1);
-        $name = substr($temp_name, 0, strpos($temp_name, '?')) .'.jpg';
+        $name = substr($temp_name, 0, strpos($temp_name, '?')) . '.jpg';
         $path = 'images/' . $name;
-        Storage::put('images/'.$name, $contents);
+        Storage::put('images/' . $name, $contents);
         return $path;
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <ul>
         @foreach ($products as $product)
-            <li>{{ $product->name }}</li>
+            <li>{{ $product->name }} <img src="{{ asset('storage/' . $product->cover_image) }}" alt="" width="100" height="100"></li>
         @endforeach
     </ul>
 @endsection
