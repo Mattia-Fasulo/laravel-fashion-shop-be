4619d2bbb08bbf973b5cd0d47d32f03001ff57ec
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-01-23 11:54:44 +0100

    add migrate brand, texture and type

---
+++ app/Http/Controllers/TextureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\texture;
+use Illuminate\Http\Request;
+
+class TextureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\texture  $texture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(texture $texture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\texture  $texture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(texture $texture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\texture  $texture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, texture $texture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\texture  $texture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(texture $texture)
+    {
+        //
+    }
+}
