f384dddc2e68ffceb51e40bd7483d7cf98d4b6c6
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-01-24 12:44:37 +0100

    index and show view basic completed

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="my-5" id="indexProduct">
         <div class="container post-list">
             @if(session()->has('message'))
                 <div class="alert alert-success text-primary mb-3 mt-3">
@@ -17,17 +17,23 @@
                         <div class="card-body">
                             <ul class="container-fluid">
                                 @foreach ($products as $product)
-                                    <li class="row row-cols-2">
-                                        <a href="#" title="View product" class="col-10">
-                                            {{$product->id}} {{$product->name}} <span class="text-capitalize">{{$product->brand->name}}</span>
-                                            @if ($product->type_id)
-                                                <sub ><span title="Type">{{$product->type->name}}</span></sub>
-                                            @endif
+                                    <li class="row row-cols-2 my-1">
+                                        <a href="{{route('admin.products.show', $product->slug)}}" title="View product" class="col-10">
+                                            {{$product->id}} {{$product->name}} <span class="text-capitalize brand" title="Brand: {{$product->brand->name}}">{{$product->brand->name}}</span>
+                                            <sub class="text-capitalize">
+                                                @if ($product->type_id)
+                                                    <span title="Type">{{$product->type->name}}</span>
+                                                @endif
+                                                @if ($product->texture_id)
+                                                    <span title="Texture">{{$product->texture->name}}</span>
+                                                @endif
+                                            </sub>
+                                            
                                         </a> 
 
                                         {{-- edit part --}}
                                         <div class="edit col-2 row row-cols-2">
-                                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-pencil"></i></a>
+                                            <a href="#" class="btn btn-primary col-auto"><i class="fa-solid fa-pencil"></i></a>
                                             <form action="" method="post">
                                                 @csrf
                                                 @method('DELETE')
---
+++ resources/views/admin/products/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="my-5" id=showProduct>
+        <div class="container">
+            <div class="card">
+                <div class="card-header d-flex align-items-baseline justify-content-between">
+                    <h1>{{$product->name}}</h1>
+                    <div class="info d-flex">
+                        {{-- @if ($product->type_id)
+                            <div class="sub me-3" title="Type">{{$product->type_id ? $product->type->name : ''}}</div>
+                        @endif --}}
+                        <sub class="text-capitalize">
+                            @if ($product->type_id)
+                                <span title="Type">{{$product->type->name}}</span>
+                            @endif
+                            @if ($product->texture_id)
+                                <span title="Texture">{{$product->texture->name}}</span>
+                            @endif
+                        </sub>
+                    </div>
+                    
+                </div>
+                <div class="card-body">
+                    <div class="product-body row">
+                        <div class="body-text {{$product->cover_image ? 'col-4': 'col-12'}}">
+                            <div>{{$product->description}}</div>
+                            <div class="price">
+                                <h3>{{$product->price}}{{$product->price_sign}}</h3>
+                            </div>
+                        </div>
+                        @if ($product->cover_image)
+                            <div class="body-image col-4 offset-4"><img class="w-100" src="{{asset('storage/'.$product->cover_image)}}" alt=""></div>
+                        @endif
+
+                        
+                    </div>
+                </div>
+            </div>
+            <a href="{{route('admin.products.index')}}" class="btn btn-primary my-3">Show all Products</a>
+        </div>
+    </section>
+@endsection
